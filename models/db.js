7a36f3a8ebd5783deee3b3d5b4a5c9bdd4db29d7
Amarnath Natarajan <amarnath.n@domain.rage-india.com>
2023-02-23 11:02:09 +0530

    push server

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.set("strictQuery", false)
+
+module.exports = mongoose.connect('mongodb+srv://amarnath:amarnath@cluster0.xzpc6dv.mongodb.net/tasks?retryWrites=true&w=majority' , {
+    // useUnifiedTopology : true,
+    // useNewUrlParser : true,
+    // useFindAndModify : false
+    useNewUrlParser: true,
+          useUnifiedTopology: true,
+
+}, err => {
+    if(err) console.log(`error is DB connection ${err}`)
+    console.log(`mongoDB connection Success`)
+})
