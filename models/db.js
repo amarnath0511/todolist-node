8629c3aead9205e5467e4e54ce5607c7ab65e492
Amarnath Natarajan <amarnath.n@domain.rage-india.com>
2023-02-23 11:34:07 +0530

    revote model db

--- models/db.js
+++ models/db.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 mongoose.set("strictQuery", false)
 
 module.exports = mongoose.connect('mongodb+srv://amarnath:amarnath@cluster0.xzpc6dv.mongodb.net/tasks?retryWrites=true&w=majority' , {
-    useUnifiedTopology : true,
-    useNewUrlParser : true,
-    useFindAndModify : false
-    // useNewUrlParser: true,
-    //       useUnifiedTopology: true,
+    // useUnifiedTopology : true,
+    // useNewUrlParser : true,
+    // useFindAndModify : false
+    useNewUrlParser: true,
+          useUnifiedTopology: true,
 
 }, err => {
     if(err) console.log(`error is DB connection ${err}`)
